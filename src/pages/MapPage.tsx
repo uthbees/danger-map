6f042dfc6b08b40ed61e28e801ad37d6814aa1bb
Ethan Ball <33760679+uthbees@users.noreply.github.com>
2024-10-18 20:52:07 -0600

    More map UI

--- src/pages/MapPage.tsx
+++ src/pages/MapPage.tsx
@@ -1,14 +1,24 @@
-import { Stack } from '@mui/material';
+import { Paper, Stack } from '@mui/material';
 
 export default function MapPage() {
     return (
-        <Stack direction="row" style={{ height: '100%' }}>
-            <div style={{ height: '100%', minWidth: '250px' }}>Sidebar</div>
+        <Stack
+            direction="row"
+            style={{ height: '100%', backgroundColor: 'green' }}
+        >
+            <Paper
+                style={{
+                    height: '100%',
+                    minWidth: '250px',
+                    backgroundColor: 'white',
+                }}
+                elevation={6}
+                square
+            ></Paper>
             <div
                 style={{
                     height: '100%',
                     width: '100%',
-                    backgroundColor: 'green',
                 }}
             >
                 Map
