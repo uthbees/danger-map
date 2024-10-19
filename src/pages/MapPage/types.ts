f41f95d1ff66fb15e70cbcd52a76d44a2325ff55
Ethan Ball <33760679+uthbees@users.noreply.github.com>
2024-10-19 08:02:55 -0600

    Allow car crash data to be undefined

--- src/pages/MapPage/types.ts
+++ src/pages/MapPage/types.ts
@@ -2,8 +2,8 @@ export interface CountyData {
     COUNTYNAME: string;
     STATENAME: string;
     POPULATION: number;
-    FATALS: number; // Total fatalities/serious injuries from car crashes.
-    TOTAL_INCIDENTS: number; // Total car crashes.
+    FATALS?: number; // Total fatalities/serious injuries from car crashes.
+    TOTAL_INCIDENTS?: number; // Total car crashes.
 }
 
 export interface RiskFactorEnabledStatuses {
